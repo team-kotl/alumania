adef0b0230dd86b922a97055a64677dafb06b7b5
yukiroow <hcdominguez07@gmail.com>
2025-03-01 20:44:45 +0800

    Upgrade tailwind and daisyui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,5 @@
-import daisyui from "daisyui"
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#032543",
-          secondary: "#0065E8",
-          accent: "#93c5fd",
-          neutral: "#1f2937",
-          "base-100": "#F6FBFF",
-          info: "#60a5fa",
-          success: "#00ff00",
-          warning: "#ea580c",
-          error: "#ff0000",
-          text: "#343434",
-        },
-      },
-    ],
-  },
-  plugins: [daisyui],
+  plugins: [],
 };
